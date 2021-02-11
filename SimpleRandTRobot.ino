2fc0259b3bd81de714244a0dcda83905e1c3a7f2
monitronica <78903881+monitronica@users.noreply.github.com>
2021-02-11 22:52:33 +0100

    Add files via upload

---
+++ SimpleRandTRobot.ino
@@ -0,0 +1,34 @@
+/*
+ * The code first defines the pins of the servo motors and 
+ * potentiometers. It reads the value of the potentiometer, 
+ * scales it to use it with the servos and sets their 
+ * position according to the scaled values.
+ */
+#include <Servo.h>
+
+Servo RotateServo; 
+Servo TiltServo; 
+
+int RotatePot = A2;  
+int valueR;   
+
+int TiltPot = A0;  
+int valueT;   
+
+void setup()
+{
+  RotateServo.attach(9); 
+  TiltServo.attach(10); 
+}
+
+void loop() 
+{ 
+  valueR = analogRead(RotatePot);            
+  valueR = map(valueR, 0, 1023, 0, 180);     
+  valueT = analogRead(TiltPot);            
+  valueT = map(valueT, 0, 1023, 0, 180);
+  RotateServo.write(valueR);                  
+  TiltServo.write(valueT);
+  delay(15);                           
+} 
+
